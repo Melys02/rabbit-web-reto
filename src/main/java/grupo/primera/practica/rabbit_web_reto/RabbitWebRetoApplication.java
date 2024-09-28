78a44fbe4205f12de8712e73917eab2404c5741f
Melys02 <melys0919@gmail.com>
2024-09-27 20:53:39 -0500

    inicio de proyecto

---
+++ src/main/java/grupo/primera/practica/rabbit_web_reto/RabbitWebRetoApplication.java
@@ -0,0 +1,13 @@
+package grupo.primera.practica.rabbit_web_reto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitWebRetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitWebRetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grupo/primera/practica/rabbit_web_reto/RabbitWebRetoApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo.primera.practica.rabbit_web_reto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitWebRetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
