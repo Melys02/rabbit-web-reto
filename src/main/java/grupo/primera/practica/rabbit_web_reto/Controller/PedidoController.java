af91965388054588d6b5f4f691a91062f21c459c
zchanway <146023170+zchanway@users.noreply.github.com>
2024-09-27 22:17:27 -0500

    controller

--- src/main/java/grupo/primera/practica/rabbit_web_reto/Controller/PedidoController.java
+++ src/main/java/grupo/primera/practica/rabbit_web_reto/Controller/PedidoController.java
@@ -1,9 +1,23 @@
 package grupo.primera.practica.rabbit_web_reto.Controller;
 
+import grupo.primera.practica.rabbit_web_reto.modelo.Pedido;
+import grupo.primera.practica.rabbit_web_reto.producer.PedidoProducer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController
+@RequestMapping("/pedidos")
 public class PedidoController {
+
+    @Autowired
+    private PedidoProducer pedidoProducer;
+
+    @PostMapping
+    public void crearPedido(@RequestBody Pedido pedido) {
+        pedidoProducer.enviarPedido(pedido);
+    }
 }
