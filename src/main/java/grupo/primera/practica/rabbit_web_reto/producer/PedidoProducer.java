0a8fcad903cacc0581f5506f67180bc217439062
Melys02 <melys0919@gmail.com>
2024-09-27 21:04:35 -0500

    RabbitTemplate

--- src/main/java/grupo/primera/practica/rabbit_web_reto/producer/PedidoProducer.java
+++ src/main/java/grupo/primera/practica/rabbit_web_reto/producer/PedidoProducer.java
@@ -1,4 +1,17 @@
 package grupo.primera.practica.rabbit_web_reto.producer;
 
+import grupo.primera.practica.rabbit_web_reto.modelo.Pedido;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class PedidoProducer {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void enviarPedido(Pedido pedido) {
+        rabbitTemplate.convertAndSend("cola_pedidos", pedido);
+        System.out.println("Pedido enviado: " + pedido);
+    }
+
 }
