28243ea85597a2b7617564c9ced058a8068159d2
zchanway <146023170+zchanway@users.noreply.github.com>
2024-09-27 22:21:01 -0500

    pedidos

--- src/main/java/grupo/primera/practica/rabbit_web_reto/consumer/PedidoConsumer.java
+++ src/main/java/grupo/primera/practica/rabbit_web_reto/consumer/PedidoConsumer.java
@@ -1,6 +1,8 @@
 package grupo.primera.practica.rabbit_web_reto.consumer;
 
+import grupo.primera.practica.rabbit_web_reto.modelo.Pedido;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,8 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class PedidoConsumer {
-}
+    @RabbitListener(queues = "cola_pedidos")
+    public void recibirPedido(Pedido pedido) {
+        System.out.println("Pedido recibido:" + pedido);
+    }
+    }
