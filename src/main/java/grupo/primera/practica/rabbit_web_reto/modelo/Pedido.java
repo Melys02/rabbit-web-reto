c3fff63af2899fdd6e9051cb89e00696d717e565
Melys02 <melys0919@gmail.com>
2024-09-27 21:01:56 -0500

    Modelo Definido

--- src/main/java/grupo/primera/practica/rabbit_web_reto/modelo/Pedido.java
+++ src/main/java/grupo/primera/practica/rabbit_web_reto/modelo/Pedido.java
@@ -1,5 +1,15 @@
 package grupo.primera.practica.rabbit_web_reto.modelo;
 
 public class Pedido {
+    private String id;
+    private String producto;
+    private int cantidad;
+
+    // Constructor, Getters y Setters
+    public Pedido(String id, String producto, int cantidad) {
+        this.id = id;
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
 
 }
