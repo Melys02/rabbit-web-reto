a41465229b0d7eee2dd06eb55a8def39d65070e7
Melys02 <melys0919@gmail.com>
2024-09-27 21:21:44 -0500

    component

--- src/main/java/grupo/primera/practica/rabbit_web_reto/consumer/PedidoConsumer.java
+++ src/main/java/grupo/primera/practica/rabbit_web_reto/consumer/PedidoConsumer.java
@@ -1,8 +1,10 @@
 package grupo.primera.practica.rabbit_web_reto.consumer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+@Component
 @Slf4j
 @Service
 public class PedidoConsumer {
